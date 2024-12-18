cf2f2de6bab77c898285c0ed98dac68d67ce4b75
Deyan Tsonev <dio.tsonev@gmail.com>
2024-12-18 18:07:36 +0200

    add environments folder to angular directory

---
+++ Frontend/MusicMarketBulgaria/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5000',
+  passwordValidationLevels: ['minLength'],
+  allowedDomains: [
+    'gmail.com',
+    'example.com',
+    'yahoo.com',
+    'hotmail.com',
+    'outlook.com',
+    'abv.bg',
+  ],
+};
